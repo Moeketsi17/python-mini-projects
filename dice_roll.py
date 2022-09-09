890ffaed5c3f9629347d49d506eba1a220d52e86
moeketsi <raymondkxng17@gmail.com>
2022-09-09 19:13:23 +0200

    comiit

--- dice_roll.py
+++ dice_roll.py
@@ -3,7 +3,7 @@ import random
 min_val = 1
 max_val = 6
 
-roll_again = "yes"
+roll_again = input()
 
 while roll_again == "yes" or roll_again == "y":
     print("Dices rolling...")
@@ -13,8 +13,9 @@ while roll_again == "yes" or roll_again == "y":
     print(random.randint(min_val, max_val))
     # print randomly generated number 2nd die
     print(random.randint(min_val, max_val))
+    break
 
-roll_again = input("Roll the dice again? ")
+#roll_again = input("Roll the dice again? ")
 
 
 
